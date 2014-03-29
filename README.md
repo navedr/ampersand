162fc7c5a0d0ab9fb0f58e7aaba15cc30f924a6a
Naved Rangwala <navedindia@gmail.com>
2014-03-29 18:55:07 -0400

    added newer version of bootstrap and font awesome

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ ampersand is built using the popular libraries out there. The libraries used in 
 * jQuery Forms
 * Intro JS
 * Twitter Bootstrap
+* Underscore
 * and some custom libraries...
 
 How do I use ampersand?
@@ -23,7 +24,8 @@ Just download this library and embed the following in the `<head>` tag of your p
 	`<script type="text/javascript" src="js/ampersand.min.js"></script>`  
 and thats all you will ever need.
 
-Are there any prerequisites before using ampersand?
+Are there any prerequisites?
 ---------------------------------------------------
 The only other library which you will need to add before adding ampersand is `jQuery` but it is already included in the download. 
-If you are already using `jQuery` just add it to your page before adding ampersand. jQuery 1.9 is recommended. 
+If you are already using `jQuery` just add it to your page before adding ampersand. jQuery 1.9+ is recommended. 
+
--- js/ampersand.js
+++ js/ampersand.js
@@ -1,7 +1,7 @@
 function loadjscssfile(filename,filetype){if(filetype=="js"){var fileref=document.createElement('script');fileref.setAttribute("type","text/javascript");fileref.setAttribute("src",filename)}else if(filetype=="css"){var fileref=document.createElement("link");fileref.setAttribute("rel","stylesheet");fileref.setAttribute("type","text/css");fileref.setAttribute("href",filename)}if(typeof fileref!="undefined"){document.getElementsByTagName("head")[0].appendChild(fileref)}}
 var cssFiles = ['bootstrap/css/bootstrap.min.css', 'bootstrap/css/bootstrap-responsive.min.css', 'jquery-ui/css/jquery-ui.css',
                 'jquery-ui/smoothness/jquery-ui-1.8.17.custom.css', 'css/introjs.min.css', 'css/validation.css', 'css/fancy.form.css', 
-                'css/helper.css'];
+                'css/helper.css', 'font-awesome/css/font-awesome.min.css'];
 var jsFiles = ['jquery/jquery.form.min.js', 'jquery-ui/js/jquery-ui.js', 'js/intro.min.js', 
                'bootstrap/js/bootstrap.min.js', 'js/underscore-min.js'];
 for(var x in cssFiles){
