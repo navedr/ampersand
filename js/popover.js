6384a4f9cac8076bad0cd7267003b88fa8568d50
DealEvo Mac <itdept@dealevo.com>
2014-03-28 23:35:02 -0400

    adding libraries

---
+++ js/popover.js
@@ -0,0 +1,36 @@
+﻿Popover = {
+    create: function (linkId, title, content, container, placement) {
+        title = title + "<button class='close' onclick=\"$$('#" + linkId + "').popover('hide');\">&times;</button>";
+        $$('#' + linkId).popover({
+            placement: (placement) ? placement : 'top',
+            html: true,
+            content: content,
+            container: (container) ? container : false,
+            title: title
+        });
+    },
+    bindEvents: function () {
+        $(".popover .close-popover").live("click", function (e) {
+            var $btn = $(e.target);
+            $btn.parents(".popover").hide();
+            return false;
+        });
+        $(".popover-trigger").live("click", function (e) {
+            var $el = $(e.target);
+            if (!$el.hasClass("popover-trigger")) {
+                $el = $el.parents(".popover-trigger");
+            }
+            if ($el.data("popoverId") != null) {
+                $("#" + $el.data("popoverId")).toggle();
+                return false;
+            }            
+        });
+    },
+    init: function(){
+        this.bindEvents();
+    }
+}
+
+$(document).ready(function () {
+    Popover.init();
+});
---
+++ js/topbar.js
@@ -0,0 +1,37 @@
+﻿TopBar = {
+    init: function () {
+        this.bindEvents();
+    },
+    bindEvents: function () {
+        $(".top-bar .top-bar-toggle").live("click", $.proxy(this.toggleTopBar, this));
+    },
+    toggleTopBar: function (e) {
+        var $el = $(e.target);
+        if (!$el.hasClass("top-bar-toggle")) {
+            $el = $el.parents(".top-bar-toggle");
+        }
+        var $content = $el.parents(".top-bar").find(".top-bar-content");
+        $content.slideToggle({
+            duration: 300,
+            easing: 'easeInOutBack',
+            complete: function () {
+                var visible = $content.is(":visible")
+                if (visible) {
+                    if ($el.data("visible")) {
+                        $el.html($el.data("visible"))
+                    }
+                }
+                else {
+                    if ($el.data("hidden")) {
+                        $el.html($el.data("hidden"))
+                    }
+                }
+            }
+        });
+        return false;
+    }
+};
+
+$(document).ready(function () {
+    TopBar.init();
+});
